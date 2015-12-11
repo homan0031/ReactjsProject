818f13a0ba275c10dd3ae43b6ae82388fd2e7547
Homan <homan0011@gmail.com>
2015-12-11 17:17:22 +0800

    Add React NavBar
    Finish React NavBar Sample

---
+++ javascript/my-jsx.js
@@ -0,0 +1,75 @@
+var NavDropDownList = React.createClass({
+	render: function(){
+		var createItem = function(items){
+			return (
+				<li key={items}>
+					<a href="#">{items}</a>
+				</li>
+			);
+		};
+
+		return <ul className="dropdown-menu">{this.props.items.map(createItem)}</ul>
+	}
+});
+
+var NavBar = React.createClass({
+	getInitialState: function(){
+		return {items: ["Chapter A", "Chapter B"]};
+	},
+
+	render: function(){
+		return (
+			<nav className="navbar navbar-default">
+				{/*Brand and toggle get grouped for better mobile display*/}
+				<div className="navbar-header">
+					<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navMenuList" aria-expanded="false">
+						<span className="icon-bar" />
+						<span className="icon-bar" />
+						<span className="icon-bar" />
+					</button>
+					<a className="navbar-brand" href="#">TMS</a>
+				</div>
+
+				<div className="collapse navbar-collapse" id="navMenuList">
+		      <ul className="nav navbar-nav">
+		      	<li className="dropdown">
+  						<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">TMS Review<span className="caret"></span></a>
+  						<NavDropDownList items={this.state.items} />
+						</li>
+		        <li>
+		        	<a href="#">Random Test</a>
+		        </li>
+	
+		      </ul>
+		    </div>
+			</nav>
+		);
+	}
+});
+
+var Jumbotron = React.createClass({
+	render: function(){
+		return(
+			<div className="jumbotron">
+			  <div className="container">
+			    <h1>Hello, world!</h1>
+			    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+			    <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+			  </div>
+			</div>
+		);
+	}
+});
+
+var FirstPage = React.createClass({
+	render: function(){
+		return(
+			<div className="web-container">
+				<NavBar/>
+				<Jumbotron/>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<FirstPage/>, document.getElementById("content"));
---
+++ javascript/my-react.js
@@ -0,0 +1,134 @@
+"use strict";
+
+var NavDropDownList = React.createClass({
+	displayName: "NavDropDownList",
+
+	render: function render() {
+		var createItem = function createItem(items) {
+			return React.createElement(
+				"li",
+				{ key: items },
+				React.createElement(
+					"a",
+					{ href: "#" },
+					items
+				)
+			);
+		};
+
+		return React.createElement(
+			"ul",
+			{ className: "dropdown-menu" },
+			this.props.items.map(createItem)
+		);
+	}
+});
+
+var NavBar = React.createClass({
+	displayName: "NavBar",
+
+	getInitialState: function getInitialState() {
+		return { items: ["Chapter A", "Chapter B"] };
+	},
+
+	render: function render() {
+		return React.createElement(
+			"nav",
+			{ className: "navbar navbar-default" },
+			React.createElement(
+				"div",
+				{ className: "navbar-header" },
+				React.createElement(
+					"button",
+					{ type: "button", className: "navbar-toggle collapsed", "data-toggle": "collapse", "data-target": "#navMenuList", "aria-expanded": "false" },
+					React.createElement("span", { className: "icon-bar" }),
+					React.createElement("span", { className: "icon-bar" }),
+					React.createElement("span", { className: "icon-bar" })
+				),
+				React.createElement(
+					"a",
+					{ className: "navbar-brand", href: "#" },
+					"TMS"
+				)
+			),
+			React.createElement(
+				"div",
+				{ className: "collapse navbar-collapse", id: "navMenuList" },
+				React.createElement(
+					"ul",
+					{ className: "nav navbar-nav" },
+					React.createElement(
+						"li",
+						{ className: "dropdown" },
+						React.createElement(
+							"a",
+							{ href: "#", className: "dropdown-toggle", "data-toggle": "dropdown", role: "button", "aria-haspopup": "true", "aria-expanded": "false" },
+							"TMS Review",
+							React.createElement("span", { className: "caret" })
+						),
+						React.createElement(NavDropDownList, { items: this.state.items })
+					),
+					React.createElement(
+						"li",
+						null,
+						React.createElement(
+							"a",
+							{ href: "#" },
+							"Random Test"
+						)
+					)
+				)
+			)
+		);
+	}
+});
+
+var Jumbotron = React.createClass({
+	displayName: "Jumbotron",
+
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "jumbotron" },
+			React.createElement(
+				"div",
+				{ className: "container" },
+				React.createElement(
+					"h1",
+					null,
+					"Hello, world!"
+				),
+				React.createElement(
+					"p",
+					null,
+					"This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
+				),
+				React.createElement(
+					"p",
+					null,
+					React.createElement(
+						"a",
+						{ className: "btn btn-primary btn-lg", href: "#", role: "button" },
+						"Learn more"
+					)
+				)
+			)
+		);
+	}
+});
+
+var FirstPage = React.createClass({
+	displayName: "FirstPage",
+
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "web-container" },
+			React.createElement(NavBar, null),
+			React.createElement(Jumbotron, null)
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(FirstPage, null), document.getElementById("content"));
+/*Brand and toggle get grouped for better mobile display*/
